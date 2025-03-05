ee86f7ca26dd5348e2034913d7dafcedcad02796
AerrowV <businessasimk@gmail.com>
2025-03-05 10:26:46 +0100

    Update

--- src/main/java/dat/daos/PoemDAO.java
+++ src/main/java/dat/daos/PoemDAO.java
@@ -7,13 +7,14 @@ import jakarta.persistence.EntityManagerFactory;
 
 import java.util.List;
 
-public class PoemDAO implements IDAO<Poem, Long>  {
+public class PoemDAO implements IDAO<Poem, Long> {
 
 
     private static EntityManagerFactory emf;
     private static PoemDAO instance = null;
 
-    private PoemDAO() {}
+    private PoemDAO() {
+    }
 
     public static PoemDAO getInstance(EntityManagerFactory _emf) {
         if (emf == null) {
@@ -23,60 +24,54 @@ public class PoemDAO implements IDAO<Poem, Long>  {
         return instance;
     }
 
-
     @Override
     public Poem create(Poem poem) {
-        try(EntityManager em = emf.createEntityManager()) {
-        em.getTransaction().begin();
-        em.persist(poem);
-        em.getTransaction().commit();
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(poem);
+            em.getTransaction().commit();
 
-        }catch(Exception e) {
-        new ApiException(400, "poem was not created");
-    }
+        } catch (Exception e) {
+            new ApiException(400, "poem was not created");
+        }
         return poem;
-}
-
+    }
 
     @Override
     public List<Poem> readAll() {
-        try(EntityManager _em = emf.createEntityManager()) {
+        try (EntityManager _em = emf.createEntityManager()) {
             return _em.createQuery("SELECT s from Poem s", Poem.class).getResultList();
         }
     }
 
-
-
     @Override
     public Poem read(Long id) {
-        try(EntityManager _em = emf.createEntityManager()) {
+        try (EntityManager _em = emf.createEntityManager()) {
             return _em.find(Poem.class, id);
         }
     }
 
-
     @Override
     public Poem update(Poem poem) {
-        try(EntityManager em = emf.createEntityManager()) {
+        try (EntityManager em = emf.createEntityManager()) {
             em.getTransaction().begin();
             em.merge(poem);
             em.getTransaction().commit();
 
-        }catch (Exception e) {
-            new ApiException(400,"poem wasnt updated");
+        } catch (Exception e) {
+            new ApiException(400, "poem wasnt updated");
         }
         return poem;
     }
 
     @Override
     public void delete(Long id) {
-        try(EntityManager _em = emf.createEntityManager()) {
+        try (EntityManager _em = emf.createEntityManager()) {
             _em.getTransaction().begin();
             _em.remove(read(id));
             _em.getTransaction().commit();
-        }catch (Exception e) {
-            new ApiException(400,"poem wasnt deleted");
+        } catch (Exception e) {
+            new ApiException(400, "poem wasnt deleted");
         }
-
     }
 }
