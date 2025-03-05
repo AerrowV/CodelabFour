7fef29e0341b7020293a56abafa0525dca51e82d
AerrowV <businessasimk@gmail.com>
2025-03-05 10:58:08 +0100

    Update

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,7 +1,23 @@
 package dat;
 
+import dat.config.HibernateConfig;
+import dat.daos.PoemDAO;
+import dat.dtos.PoemDTO;
+import dat.entities.Poem;
+import dat.services.PoemImporter;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
+
 public class Main {
+
+    private static final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+    private static final PoemDAO POEM_DAO = PoemDAO.getInstance(emf);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Poem> importedPoems = PoemImporter.importPoemsFromJson(emf);
+        System.out.println(importedPoems);
+
     }
 }
